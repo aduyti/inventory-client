63a6ed85e6e221f08c48910966123baca3e08bc1
Anirudhya <anirudhyaduyti@gmail.com>
2022-05-07 17:08:22 +0600

    product card

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+    const { _id, name, description, price, quantity, supplier, image } = product;
+    const navigate = useNavigate();
     return (
-        <div>
-
-        </div>
+        <div className="border-2 rounded-lg border-gray-400 w-64 h-[400px] mx-auto mb-8 bg-white">
+            <h3 className="text-2xl font-bold">{name}</h3>
+            <img src={image} alt={name} className="h-40 w-full rounded" />
+            <div className="text-left pl-3 text-slate-500 py-3">
+                <h4 className="text-xl font-semibold ">Supplier : {supplier}</h4>
+                {
+                    description.map(line => <p className="text-sm" key={line}>{line}</p>)
+                }
+            </div>
+            <div className="flex justify-between px-5 pb-3">
+                <h4 className="text-xl font-semibold text-slate-500">${price}</h4>
+                <h4 className="text-xl font-semibold text-slate-500">{quantity}pcs</h4>
+            </div>
+            <button type="button"
+                className="w-full bg-cyan-500 rounded-b font-semibold py-1 "
+                onClick={() => {
+                    navigate(`./inventory/${_id}`)
+                }}>Manage</button>
+        </div >
     );
 };
 
--- src/Components/ProductsSection/ProductsSection.js
+++ src/Components/ProductsSection/ProductsSection.js
@@ -4,11 +4,10 @@ import ProductCard from '../ProductCard/ProductCard'
 
 const ProductsSection = () => {
     const [products] = use6Products();
-    console.log(products);
     return (
         <>
             <h2 className="text-3xl font-bold text-cyan-600 py-8">Our Collection</h2>
-            <div>
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-8">
                 {
                     products.map((product) => <ProductCard key={product._id} product={product} />)
                 }
